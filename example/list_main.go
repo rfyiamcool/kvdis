9baf5e6c8222630343f8d2afc11cd34ce8e335a0
rfyiamcool <rfyiamcool@163.com>
2017-12-05 17:08:18 +0800

    add list example

---
+++ example/list_main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rfyiamcool/kvdis/rocks"
+)
+
+func main() {
+	var err error
+	var res []byte
+
+	rocksdb := rocks.NewRocksDB("./rocks_data")
+	db := rocks.New(rocksdb)
+	defer db.Close()
+
+	l := db.List([]byte("qq"))
+
+	pushTs := time.Now()
+	for index := 0; index < 1000000; index++ {
+		l.RPush([]byte(fmt.Sprintf("xiaorui.cc index: %d", index)))
+	}
+
+	fmt.Printf("push cost time: %v", time.Since(pushTs))
+
+	fmt.Printf("queue length: %d \n", l.Len())
+
+	for index := 0; index < 1; index++ {
+		res, err = l.LPop()
+		fmt.Printf("LPOP value: %v, err: %v \n", string(res), err)
+	}
+
+	fmt.Printf("queue length: %d \n", l.Len())
+
+	for _, v := range db.Keys() {
+		fmt.Printf("has key name: %s\n", v)
+	}
+
+	l.Drop()
+}
